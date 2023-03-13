1e15647cb8850938aae0fa197b4ffe137e04fb8b
SashaKorobkina <s.dekaiper@gmail.com>
2023-03-13 11:59:44 +0000

    modified Contact.js and added styles to Contact.css, add CV-Sasha-Zhyliuk-short.pdf file to pudlic folder

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,27 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import { DiGithubBadge } from "react-icons/di"
+import { AiOutlineGithub } from "react-icons/ai"
 import { ImLinkedin } from "react-icons/im"
 import { SiGmail } from "react-icons/si"
-import { BsFillTelephoneInboundFill } from "react-icons/bs"
+import { AiFillFilePdf } from "react-icons/ai"
+import "../styles/Contact.css"
 
 
 function Contact() {
 	return(
 		<section className="contact">
 			<ul className="contacts">
-				<h1>Contact Me</h1>
+				<h1>My Contacts:</h1>
 				<li>
-					<Link to="#"><ImLinkedin className="contact-icon"></ImLinkedin>My Linked In</Link>
+					<Link className="contact-link" to="#"><ImLinkedin className="contact-icon">
+						</ImLinkedin>
+						My Linked In
+					</Link>
 				</li>
 				<li>
-					<Link to="#"><DiGithubBadge className="contact-icon"></DiGithubBadge>My Git Hub</Link>
+					<Link className="contact-link" to="#">
+						<AiOutlineGithub className="contact-icon"></AiOutlineGithub>
+						My Git Hub
+					</Link>
 				</li>
 				<li>
-					<span><SiGmail className="contact-icon"></SiGmail>My Email:</span>
+					<Link className="contact-link" to="mailto:sDekaiper@gmail.com">
+						<SiGmail className="contact-icon"></SiGmail>
+							My Email
+					</Link>
 				</li>
 				<li>
-					<span><BsFillTelephoneInboundFill className="contact-icon"></BsFillTelephoneInboundFill>My Contact Phone:</span>
+					<Link className="contact-link" to="../../public/CV-Sasha-Zhyliuk-short.pdf" download>
+							<AiFillFilePdf className="contact-icon"></AiFillFilePdf>
+							My CV	
+					</Link>
 				</li>
 			</ul>
 		</section>
